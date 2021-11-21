412e2aefbde78a717cf8e371927e5720f65d39b5
FANTASTPATR <317297062@qq.com>
2021-11-21 20:22:14 +0800

    add lr schedule
    添加lr衰减

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ from torch.utils.tensorboard import SummaryWriter
 parser = argparse.ArgumentParser()
 
 # ---for training----
-parser.add_argument("--device", type=str, default="cuda:6")
-parser.add_argument('--data', type=str, default='PEMS-D3', help='dataset')
+parser.add_argument("--device", type=str, default="cuda:1")
+parser.add_argument('--data', type=str, default='PEMS-D8', help='dataset')
 parser.add_argument('--batch_size', type=int, default=32, help='batch size')
 parser.add_argument('--epochs', type=int, default=500, help='training epoch')
 parser.add_argument("--seed", type=int, default=520, help='random seed')
 parser.add_argument("--clip", type=float, default=5., help='gradient clip')
-parser.add_argument("--lr", type=float, default=0.0001, help='learning rate')
+parser.add_argument("--lr", type=float, default=0.001, help='learning rate')
 parser.add_argument("--dropout", type=float, default=0.2, help='dropout rate')
 parser.add_argument('--weight_decay', type=float, default=0.000001, help='weight decay rate')
-parser.add_argument("--comment", type=str, default="PEMS-D3",
+parser.add_argument("--comment", type=str, default="PEMS-D8_lr*10_exp_decay0.99",
                     help='whether recording')
 parser.add_argument("--recording", type=bool, default=True, help='whether recording')
 
@@ -286,6 +286,7 @@ def main():
         # torch.save(engine.model.state_dict(), './parameter_12.pkl')
         print("Average Training Time: {:.4f} secs/epoch".format(np.mean(train_time)))
         print("Average Inference Time: {:.4f} secs".format(np.mean(val_time)))
+        engine.lr_sch.step()
 
 
 if __name__ == '__main__':
