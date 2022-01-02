b061f5842b6dc48c99841ebcdaf10609710e4357
FANTASTPATR <317297062@qq.com>
2022-01-03 00:47:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ from torch.utils.tensorboard import SummaryWriter
 parser = argparse.ArgumentParser()
 
 # ---for training----
-parser.add_argument("--device", type=str, default="cuda:6")
-parser.add_argument('--data', type=str, default='PEMS-D3', help='dataset')
-parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+parser.add_argument("--device", type=str, default="cuda:0")
+parser.add_argument('--data', type=str, default='PEMS-D7', help='dataset')
+parser.add_argument('--batch_size', type=int, default=8, help='batch size')
 parser.add_argument('--epochs', type=int, default=500, help='training epoch')
 parser.add_argument("--seed", type=int, default=42, help='random seed')
 parser.add_argument("--clip", type=float, default=5., help='gradient clip')
 parser.add_argument("--lr", type=float, default=0.001, help='learning rate')
 parser.add_argument("--dropout", type=float, default=0.2, help='dropout rate')
 parser.add_argument('--weight_decay', type=float, default=0.000001, help='weight decay rate')
-parser.add_argument("--comment", type=str, default="D3_adaptive",
+parser.add_argument("--comment", type=str, default="D7_adaptive",
                     help='whether recording')
 parser.add_argument("--recording", type=bool, default=True, help='whether recording')
 
